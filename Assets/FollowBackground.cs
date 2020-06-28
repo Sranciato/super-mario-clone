2dbc39aca11cd9150ae7b7a4f5e8093a86c5ba38
sranciato <sranciato1@gmail.com>
2020-06-28 01:30:46 -0400

    [INIT]

---
+++ Assets/FollowBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Camera.main.transform.position.x + 23f, transform.position.y, 40f);
+    }
+}
---
+++ Assets/GameOverMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT GAME");
+    }
+
+}
